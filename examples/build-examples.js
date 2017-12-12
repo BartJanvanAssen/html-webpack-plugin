0de5f83f3488e36675b5a87231d9a0bb7012c8ca
Graham McGregor <graham@gmcgregor.ca>
2017-12-11 19:02:26 -0500

    Workaround for file-loader

--- examples/build-examples.js
+++ examples/build-examples.js
@@ -30,8 +30,17 @@ examples.forEach(function (exampleName) {
   var examplePath = path.join(__dirname, exampleName);
   var configFile = path.join(examplePath, 'webpack.config.js');
 
+  var config = require(configFile);
+  if (webpackMajorVersion === '4') {
+    config.plugins.unshift(new webpack.LoaderOptionsPlugin({
+      options: {
+        context: process.cwd() // or the same value as `context`
+      }
+    }));
+  }
+
   rimraf.sync(path.join(examplePath, 'dist', 'webpack-' + webpackMajorVersion));
-  webpack(require(configFile), function (err, stats) {
+  webpack(config, function (err, stats) {
     if (err) {
       console.error(err.stack || err);
       if (err.details) {
